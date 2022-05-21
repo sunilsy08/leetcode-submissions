c0d846d4e01623c69bc844b04bb4063f26c87abd
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2022-05-21 14:41:13 +0530

    Time: 32 ms (12.63%), Space: 8.2 MB (29.67%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,14 +1,19 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
-unordered_map<char, int> m = { { 'I' , 1 },
-                                   { 'V' , 5 },
-                                   { 'X' , 10 },
-                                   { 'L' , 50 },
-                                   { 'C' , 100 },
-                                   { 'D' , 500 },
-                                   { 'M' , 1000 } };
+
+    
+    
     
     int romanToInt(string s) {
+        unordered_map<char, int> m;
+    m['I'] = 1;
+    m['V'] = 5;
+    m['X'] = 10;
+    m['L'] = 50;
+    m['C'] = 100;
+    m['D'] = 500;
+    m['M'] = 1000;
         if(s.length() == 0) return 0;
         
         int num = 0;
