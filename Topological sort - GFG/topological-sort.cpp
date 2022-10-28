bfbd4ecd4cea33046a8f34f1ab0a953b1ade9069
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2022-10-28 14:46:27 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -17,20 +17,58 @@ class Solution
 	    }
 	    st.push(node);
 	}
+
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    stack<int>st;
-	    int visited[V] = {0};
 	    
-	    for(int i=0;i<V;i++) {
-	        if(!visited[i]) {
-	            dfs(i,adj,visited,st);
-	        }
-	    }
+	   /* FOR DFS ALGO */
+	   // stack<int>st;
+	   // int visited[V] = {0};
+	    
+	   // for(int i=0;i<V;i++) {
+	   //     if(!visited[i]) {
+	   //         dfs(i,adj,visited,st);
+	   //     }
+	   // }
+	   // vector<int>ans;
+	   // while(!st.empty()) {
+	   //     ans.push_back(st.top());
+	   //     st.pop();
+	   // }
+	   // return ans;
+	   
+	   
+	   /* FOR KAHN'S ALGORITHM*/
+	   return kahns_algo_bfs(V, adj);
+
+	   
+	}
+	
+		
+	vector<int> kahns_algo_bfs(int V, vector<int> adj[]) {
+	   int indegree[V] = {0};
+	   for(int i=0;i<V;i++) {
+	       int count = 0;
+	       for(auto it: adj[i]) {
+	           indegree[it] ++;
+	       }
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++) {
+	       if(indegree[i] == 0) {
+	           q.push(i);
+	       }
+	   }
 	    vector<int>ans;
-	    while(!st.empty()) {
-	        ans.push_back(st.top());
-	        st.pop();
+	    while(!q.empty()) {
+	        int node = q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto it: adj[node]) {
+	            if(--indegree[it] == 0) {
+	                q.push(it);
+	            }
+	        }
 	    }
 	    return ans;
 	}
