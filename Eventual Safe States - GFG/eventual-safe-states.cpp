9527c3823d9d15bcd4359b08301e0514f1d99b73
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2022-10-29 12:31:36 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -33,6 +33,8 @@ class Solution {
       return false;
   }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+/*
+        USING CYCLE DETECTION (DFS)
         int visited[V] = {0};
         int pathVisited[V] = {0};
         int check[V] = {0};
@@ -48,6 +50,39 @@ class Solution {
                 ans.push_back(i);
             }
         }
+        return ans;
+        */
+        
+        // USING TOPO SORT (BFS)
+        
+        vector<int>adjrev[V];
+        int indegree[V] = {0};
+        for(int i=0;i<V;i++) {
+            for(auto it: adj[i]) {
+                adjrev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        queue<int>q;
+        for(int i=0;i<V;i++) {
+            if(indegree[i] ==0) {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adjrev[node]) {
+                indegree[it]--;
+                if(!indegree[it]) {
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
