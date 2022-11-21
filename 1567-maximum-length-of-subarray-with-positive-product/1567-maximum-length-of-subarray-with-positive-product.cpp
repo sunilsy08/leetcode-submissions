3d6bcf80ded6831dd145b53d2be7621b3f2476ef
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2022-11-21 16:58:44 +0530

    Time: 167 ms (73.22%), Space: 57.7 MB (96.60%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -1,65 +1,64 @@
 class Solution {
 public:
-    int getMaxLen(vector<int>& a) {
-      vector<int>l(a.size(), INT_MAX);
-      vector<int>r(a.size(), INT_MAX);
-        int n = a.size();
-        int lastNegPos = INT_MAX;
-        for(int i=0;i<n;i++) {
-            l[i] = lastNegPos;
-            if(a[i] <0) {
-                lastNegPos = i;
-            } else if(a[i] == 0) {
-                lastNegPos = INT_MAX;
-            }
+  int getMaxLen(vector<int>& nums) {
+        int mx=0;  //will store result
+        int sz=nums.size();
+        for(int i=0;i<sz;i++)
+        {     
+              int n_s=-1,n_e=-1,n=0,p=0,j;
+         
+              //n_s ->to store starting of negatiive number
+              //n_e ->to store end of negative number
+              //n   ->count of negative number
+              //p   ->count of positive number
+         
+         //We will iterate until we found zero 
+              for(j=i;j<sz;j++)
+              {    
+                  if(nums[j]<0) n++;
+                  if(nums[j]>0)p++;
+                  if(nums[j]<0&&n_s==-1) { n_s=j;} //store starting index of negative number
+                  if(nums[j]<0) n_e=j;             //store end index of negative number
+                  if(nums[j]==0) break;          //subarray ends now because after that all the product will be 0  
+              }
+             // cout<<i<<" "<<n_s<<" "<<n_e<<" "<<j<<" : "<<n<<" "<<p<<"\n";
+         
+             /* Here only two possibilities */
+            
+             /* 1.if we have even number of negative 
+                multiply of even negative numbers are positive, so add both n and p and take max of res 
+             */
+             if(n%2==0) mx=max(mx,n+p);
+          
+             /*
+               2.if we have negative numbers and count of these are odd ,
+               Lets say subarray -> {1,2,-1,2,3,1,-1,2,3,4,-1,2,3} , I am taking 3 negative numbers
+               We know that odd-1 = even
+               We have stored starting of negative number index in n_s
+               and end of negative number index in n_e
+               
+               For this example n_s=2 , n_e = 10
+               So here two cases, if we left a negative number of n_s index then 
+               total count of negative numbers will be odd-1=even negative numbers 
+               i.e subarray will be {2,3,1,-1,2,3,4,-1,2,3}
+               
+               Or if we left a negative number of n_e index than
+               total count of negative numbers will be odd-1=even negative numbers 
+               i.e subarray will be {1,2,-1,2,3,1,-1,2,3,4}
+               
+               So max length will be any one from these two only
+               So we will take max length of these two
+             */
+             if(n%2==1)
+             {
+                  mx=max(j-n_s-1,mx);
+                  mx=max(n_e-i,mx);
+             }
+             //cout<<mx<<" ";
+           i=j; //now starting from new subarray
         }
-        lastNegPos = INT_MAX;
-        for(int i=n-1;i>=0;i--) {
-            r[i] = lastNegPos;
-            if(a[i] <0) {
-                lastNegPos = i;
-            } else if(a[i] == 0) {
-                lastNegPos = INT_MAX;
-            }
-        }
-        int len = 0;
-        int maxlen = 0;
-        for(int i=0;i<n;i++) {
-            if(a[i] == 0) {
-                len = 0;
-                continue;
-            }
-            if(a[i] > 0) {
-                len++;
-            } 
-            else if(r[i] != INT_MAX) {
-                    len= len + (r[i] - i) + 1;
-                    i = r[i] ;
-            } else {
-                len = 0;
-                continue;
-            }
-            maxlen = max(maxlen,len);
-        }
-        len = 0;
-        for(int i=n-1;i>=0;i--) {
-            if(a[i] == 0) {
-                len = 0;
-                continue;
-            }
-            if(a[i] > 0) {
-                len++;
-            } 
-            else if(l[i] != INT_MAX) {
-                    len= len + (i-l[i]) + 1;
-                    i = l[i] ;
-            } else {
-                len = 0;
-                continue;
-            }
-            maxlen = max(maxlen,len);
-        }
+         
         
-        return maxlen;
+        return mx;
     }
 };
