b0dea19b9d419e0570594e38cbbbdc2dd396d067
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2023-09-16 13:44:28 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
+
+/* Approach 1: This will work only when input is like this:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+i.e, we are already given extra space in nums1
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int r = n-1;
         int l = m-1;
         
@@ -13,4 +19,28 @@ public:
             else nums1[index--] = nums1[l--]; 
         }
     }
+
+    */
+/*However, when input is like  [1,2,3] [2,5,6] and output : [1,2,2] [3,5,6]
+Below two approached will be used.
+*/
+
+     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int l = m-1;
+        int r = 0;
+
+        while (l>=0 && r<n) {
+            if (nums1[l] > nums2[r]) {
+                swap(nums1[l--], nums2[r++]);
+            } else {
+                break;
+            }
+        }
+        sort(&nums1[0], &nums1[m]);
+        sort(nums2.begin(),  nums2.end());
+        for(int i=0;i<n;i++) {
+            nums1[m+i] = nums2[i];
+        }
+    }
+
 };
