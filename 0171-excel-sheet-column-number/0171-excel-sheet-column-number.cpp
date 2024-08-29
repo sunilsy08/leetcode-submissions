1f717ce9b69b3d9793d0795fa2f91a568a02986c
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-08-29 22:01:01 +0530

    Time: 6 ms (5.94%), Space: 8.6 MB (64.59%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int getval(char c) {
-        return c - 'A' + 1; // Convert character to corresponding number (A = 1, B = 2, ..., Z = 26)
+        return c - 'A' + 1; 
     }
-    int titleToNumber(string columnTitle, int ind = 0) {
+    int titleToNumber(string columnTitle) {
         int n = columnTitle.size();
-        if(n-1 == ind) return getval(columnTitle[ind]);
-
-        int curr = getval(columnTitle[ind]);
-        int mul = pow(26, n - ind - 1);
-        curr *= mul;
-        curr += titleToNumber(columnTitle, ind+1);
-        return curr;
+        int result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            result = result * 26 + getval(columnTitle[i]);
+        }
+        
+        return result;
     }
 };
