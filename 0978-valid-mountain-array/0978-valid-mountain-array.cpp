0fc129dcee41bc2fa724382ca6327e773c13f9e0
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-08-29 20:21:11 +0530

    Time: 15 ms (85.66%), Space: 25.1 MB (39.59%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.cpp
+++ 0978-valid-mountain-array/0978-valid-mountain-array.cpp
@@ -3,8 +3,9 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int l = 0, n = arr.size();
         bool found = false;
-        if (n < 3) return false;
+        if (n < 3) return false; // A valid mountain array must have at least 3 elements
         
+        // Ascend until the peak
         while (l < n - 1 && arr[l] < arr[l + 1]) {
             l++;
         }
@@ -14,6 +15,7 @@ public:
         while (l < n - 1 && arr[l] > arr[l + 1]) {
             l++;
         }
+        // Check if we reached the end of the array
         return l == n - 1;
     }
 };
