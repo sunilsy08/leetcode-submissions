5bc8df3b57455ad84485691f31cd4ff86b4ae2cb
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-08-31 12:59:06 +0530

    Time: 6 ms (16.45%), Space: 10.1 MB (31.93%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-void perm(vector<int>nums, int ind,int n,vector<vector<int>>&ans, vector<int>temp ){
+void perm(vector<int>nums, int ind,int n,vector<vector<int>>&ans ){
     
     if(ind == n){
-        ans.push_back(temp); 
+        ans.push_back(nums); 
         return;
     }
     for(int i=ind; i<n; i++) {
         swap(nums[i], nums[ind]);
-        temp.push_back(nums[ind]);
-        perm(nums, ind+1, n, ans, temp);
-        temp.pop_back();
+        perm(nums, ind+1, n, ans);
         swap(nums[i], nums[ind]);
     }
 }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         int n = nums.size();
-        vector<int>temp;
-         perm(nums,0,n,ans, temp);
+         perm(nums,0,n,ans);
          return ans;
     }
 };
