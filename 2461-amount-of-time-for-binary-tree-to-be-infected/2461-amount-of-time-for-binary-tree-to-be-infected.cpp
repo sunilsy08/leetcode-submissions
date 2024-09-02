352b5148d330ff39f4a6a12591590a09014aeda7
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-09-02 19:49:53 +0530

    Time: 446 ms (35.48%), Space: 190.7 MB (17.59%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,53 +11,45 @@
  */
 class Solution {
 public:
-    void fillAdjacencyList(TreeNode* root, unordered_map<int, unordered_set<int>> &adj) {
-        if(root == NULL) return;
-        if(root->left){
-            adj[root->val].insert(root->left->val);
-            adj[root->left->val].insert(root->val);
+    void fillAdjacencyList(TreeNode* root, unordered_map<int, vector<int>> &adj) {
+        if (root == nullptr) return;
+        if (root->left) {
+            adj[root->val].push_back(root->left->val);
+            adj[root->left->val].push_back(root->val);
             fillAdjacencyList(root->left, adj);
         }
-        if(root->right){
-            adj[root->val].insert(root->right->val);
-            adj[root->right->val].insert(root->val);
+        if (root->right) {
+            adj[root->val].push_back(root->right->val);
+            adj[root->right->val].push_back(root->val);
             fillAdjacencyList(root->right, adj);
         }
     }
-    void printAdjacencyList(unordered_map<int, unordered_set<int>> adj){
-        for (const auto& pair : adj) {
-            int node = pair.first;
-            const unordered_set<int>& neighbors = pair.second;
 
-            cout << node << ": ";
-            for (int neighbor : neighbors) {
-                cout << neighbor << " ";
-            }
-            cout << "\n";
-        }
-    }
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int, unordered_set<int>> adj;
-        fillAdjacencyList(root,adj);
-        queue<pair<int,int>>q;
-        unordered_set<int>burnt;
+        unordered_map<int, vector<int>> adj;
+        fillAdjacencyList(root, adj);
+        
+        queue<pair<int, int>> q;
+        unordered_set<int> burnt;
 
-        q.push({start,0});
+        q.push({start, 0});
         burnt.insert(start);
-        int maxtime = 0;
-        while(!q.empty()) {
+        int maxTime = 0;
+
+        while (!q.empty()) {
             int node = q.front().first;
             int time = q.front().second;
-            maxtime = max(maxtime, time);
             q.pop();
-            for(auto it: adj[node]){
-                if(burnt.find(it) == burnt.end()) {
-                    q.push({it,time+1});
-                    burnt.insert(it);
+            maxTime = max(maxTime, time);
+
+            for (int neighbor : adj[node]) {
+                if (burnt.find(neighbor) == burnt.end()) {
+                    q.push({neighbor, time + 1});
+                    burnt.insert(neighbor);
                 }
             }
         }
 
-        return maxtime;
+        return maxTime;
     }
 };
