8902b0cfc86c01bfd5520cb097e43825358e2bef
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-09-28 13:14:35 +0530

    Time: 15 ms (33.42%), Space: 22.9 MB (96.25%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -10,13 +10,26 @@ public:
         if(!isLeftFlower && !isRightFlower && flowerbed[i] == 0){
             flowerbed[i] = 1;
             l = 1 + helper(flowerbed, i+2, n);
-            // flowerbed[i] = 0;
         }
         r = helper(flowerbed, i+1, n);
+        if(l>=r) cout<<" l > r \n";
+        else cout<<" r > l \n";
         return max(l,r);
     }
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int ans = 0;
-        return helper(flowerbed, 0, n) >= n;
+        int count = 0;
+        // return helper(flowerbed, 0, n) >= n;
+
+        for(int i=0; i<flowerbed.size(); i++){
+            bool leftFlower = i==0?false: flowerbed[i-1] == 1;
+            bool rightFlower = i == flowerbed.size()-1? false: flowerbed[i+1] == 1;
+
+            if(flowerbed[i] == 0 && !leftFlower && !rightFlower){
+                count++;
+                flowerbed[i] = 1;
+            }
+        }
+        return count >=n;
     }
+       
 };
