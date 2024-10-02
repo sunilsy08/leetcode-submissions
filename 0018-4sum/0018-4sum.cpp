d72572f70a20daf2d0b2e734fbbfb7c6d8634a42
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-02 16:03:22 +0530

    Time: 27 ms (56.89%), Space: 16.1 MB (72.85%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-  vector<vector<int>> fourSum(vector<int>& arr, int target) {
-    vector<vector<int>> res;
-    int n = arr.size();
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        int n = nums.size();
 
-    // Sort the array
-    sort(arr.begin(), arr.end());
-
-    // Generate quadruplets
-    for (int i = 0; i < n; i++) {
-      
-        // Skip duplicates for i
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
-
-        for (int j = i + 1; j < n; j++) {
-            // Skip duplicates for j
-            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
-
-            int k = j + 1, l = n - 1;
-
-            // Two pointers approach
-            while (k < l) {
-                long long sum = (long)arr[i] + (long)arr[j] + (long)arr[k] + (long)arr[l];
-                if (sum == target) {
-                    res.push_back({arr[i], arr[j], arr[k], arr[l]});
-                    k++;
-                    l--;
-
-                    // Skip duplicates for k and l
-                    while (k < l && arr[k] == arr[k - 1]) k++;
-                    while (k < l && arr[l] == arr[l + 1]) l--;
-                } else if (sum < target) {
-                    k++;
-                } else {
-                    l--;
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1, l = nums.size()-1;
+                while(k<l){
+                long long sum = (long)nums[i] + (long)nums[j] + (long)nums[k] + (long)nums[l];
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++; l--;
+                        while(k<nums.size() && nums[k] == nums[k-1])k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum > target){
+                        l--;
+                    } else {
+                        k++;
+                    }
                 }
             }
         }
+        return ans;
     }
-    return res;
-}
 };
