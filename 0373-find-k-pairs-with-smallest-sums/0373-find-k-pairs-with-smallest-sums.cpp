c10fd6f431e6265c9f42cd06a4fe070a2154f3cb
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-04 23:44:50 +0530

    Time: 201 ms (54.52%), Space: 147.1 MB (23.01%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,44 +1,38 @@
+class comp{
+    public:
+    bool operator()(pair<int,pair<int,int>>p1,pair<int,pair<int,int>>p2 ){
+        return p1.first < p2.first ? false:true;
+    }
+};
 class Solution {
 public:
-    class comp{
-        public:
-        bool operator()(pair<int,pair<int,int>>&p1, pair<int,pair<int,int>>&p2){
-            int sum1 = p1.first;
-            int sum2 = p2.first;
-            return (sum1 > sum2);
-        }
-    };
-    // class comp {
-    // public:
-    //     bool operator()(pair<int, int>& p1, pair<int, int>& p2) {
-    //         int sum1 = p1.first + p1.second;
-    //         int sum2 = p2.first + p2.second;
-    //         // Return true if p1 has a greater sum than p2 (min-heap behavior)
-    //         return sum1 > sum2;
-    //     }
-    // };
-    
+
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, comp> pq;
-        // priority_queue<pair<int,int>,comp>pq;
-          vector<vector<int>> ans;
+        // priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,pair<int,int>>>pq;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,
+        comp>pq;
+        // priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
 
-          for(int i=0; i<nums1.size(); i++){
-            pq.push({nums1[i] + nums2[0], {i,0}});
-          }
+        vector<vector<int>>ans;
 
-          while(!pq.empty() && k>0){
+        for(int i=0; i<nums2.size(); i++){
+            pq.push({nums1[0]+nums2[i], {0,i}});
+            // if(pq.size() >k){
+            //     pq.pop();
+            // }
+        }
+
+        while(!pq.empty() && k>0){
             int sum = pq.top().first;
             int i = pq.top().second.first;
             int j = pq.top().second.second;
+            ans.push_back({nums1[i], nums2[j]});
             pq.pop();
             k--;
-            vector<int>curr = {nums1[i],nums2[j]};
-            ans.push_back(curr);
-            if(j+1<nums2.size()){
-                pq.push({nums1[i] + nums2[j+1], {i,j+1}});
+            if(i+1 < nums1.size()){
+                pq.push({nums1[i+1] + nums2[j], {i+1,j}});
             }
-          }
-          return ans;
+        }
+        return ans;
     }
 };
