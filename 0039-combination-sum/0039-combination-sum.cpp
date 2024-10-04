112e970411b1ccaeb1671b56dea658bfb59ed798
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-04 17:24:16 +0530

    Time: 45 ms (17.13%), Space: 40.5 MB (18.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -9,13 +9,16 @@ public:
         if(currSum > target || ind >= candidates.size()){
             return;
         }
-        curr.push_back(candidates[ind]);
-        helper(candidates, target, currSum + candidates[ind], ans, ind, curr);
-        curr.pop_back();
+        if(currSum + candidates[ind] <= target){
+            curr.push_back(candidates[ind]);
+            helper(candidates, target, currSum + candidates[ind], ans, ind, curr);
+            curr.pop_back();
+        }
         helper(candidates, target, currSum, ans, ind+1, curr);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
+        vector<vector<int>>memo(target+1, vector<int>(candidates.size()+1));
         vector<int>curr;
         int currSum = 0;
         helper(candidates, target, currSum, ans,0,curr);
