c40bb3cb5fefc6bd6a9071cfe51380259e019c09
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-05 21:08:00 +0530

    Time: 6 ms (7.37%), Space: 11.3 MB (68.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-
-
-/* Approach 1: This will work only when input is like this:
-Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-Output: [1,2,2,3,5,6]
-i.e, we are already given extra space in nums1
-
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int r = n-1;
-        int l = m-1;
-        
+        int r1 = m-1, r2 = n-1;
         int index = m+n-1;
-        while(r>=0 || l>=0 ) {
-            if(r<0) nums1[index--] = nums1[l--];
-            else if(l<0) nums1[index--] = nums2[r--];
-            else if(nums2[r] > nums1[l]) nums1[index--] = nums2[r--];
-            else nums1[index--] = nums1[l--]; 
-        }
-    }
-
-    */
-/*However, when input is like  [1,2,3] [2,5,6] and output : [1,2,2] [3,5,6]
-Below two approached will be used.
-*/
-
-     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l = m-1;
-        int r = 0;
-
-        while (l>=0 && r<n) {
-            if (nums1[l] > nums2[r]) {
-                swap(nums1[l--], nums2[r++]);
-            } else {
-                break;
+        while(r1>=0 && r2>=0){
+            if(nums1[r1] > nums2[r2]){
+                nums1[index--] = nums1[r1--];
+            } else{
+                nums1[index--] = nums2[r2--];
             }
         }
-        sort(&nums1[0], &nums1[m]);
-        sort(nums2.begin(),  nums2.end());
-        for(int i=0;i<n;i++) {
-            nums1[m+i] = nums2[i];
+        while(r1>=0){
+            nums1[index--] = nums1[r1--];
+        }
+        while(r2>=0){
+            nums1[index--] = nums2[r2--];
         }
     }
-
 };
