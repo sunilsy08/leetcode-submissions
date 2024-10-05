82676b22f3623a99ff92995898618ca3b6f1d803
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-05 23:46:08 +0530

    Time: 36 ms (23.31%), Space: 16.4 MB (61.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int rowdel[4] = {-1,0,0,1};
-    int coldel[4] = {0, -1, +1, 0};
-    void dfs( vector<vector<char>>& grid, vector<vector<bool>>&visited,int r,int c,int n, int m){
-        if(r<0 || r>=n || c<0 || c>=m || visited[r][c] || grid[r][c] == '0'){
-            return;
-        }
+    int rowdel[4] = {-1, 0, 0, 1};
+    int coldel[4] = {0, -1, 1, 0};
+    void dfs(vector<vector<char>>& grid, int r,int c,vector<vector<bool>>&visited, int n, int m){
+        if(r<0 || c<0 || r>=n || c>=m || visited[r][c] || grid[r][c] == '0') return;
+        
         visited[r][c] = true;
         for(int i=0; i<4; i++){
-            int nr = r + rowdel[i];
+            int nr = r+rowdel[i];
             int nc = c + coldel[i];
-            dfs(grid, visited, nr, nc, n,m);
+            dfs(grid,nr,nc,visited,n,m);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>>visited(n, vector<bool>(m, false));
         int count =0;
-        int n =grid.size();
-        int m =grid[0].size();
-        vector<vector<bool>>visited(n, vector<bool>(m,false));
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!visited[i][j] && grid[i][j] =='1'){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    dfs(grid, i,j,visited, n, m);
                     count++;
-                    dfs(grid, visited,i,j,n,m);
                 }
             }
         }
