2fc6de8b5c50d13f1356c735fa09ad08fa6a7134
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-05 14:11:33 +0530

    Time: 189 ms (25.92%), Space: 113 MB (15.33%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,50 +1,38 @@
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
-#include <cstdlib>
-
 class RandomizedCollection {
 public:
-   vector<int> data;
-    unordered_map<int, unordered_set<int>> indices;
-
+    vector<int>data;
+    unordered_map<int,unordered_set<int>>indices;
     RandomizedCollection() {
+        
     }
-
+    
     bool insert(int val) {
-        bool isNew = (indices[val].size() == 0);
-        indices[val].insert(data.size()); 
-        data.push_back(val);          
+        bool isNew = indices[val].size() == 0;
+        data.push_back(val);
+        indices[val].insert(data.size()-1);
         return isNew;
     }
-
-
+    
     bool remove(int val) {
-        if (indices[val].empty()) return false;  
+        if(indices[val].empty()) return false;
 
-        int remove_idx = *indices[val].begin();
-        indices[val].erase(remove_idx);  
+        int removingIndex = *indices[val].begin();
+        indices[val].erase(removingIndex);
+        int lastElement = data.back();
 
-        int lastElement = data.back();  
-
-        data[remove_idx] = lastElement;
-
-        indices[lastElement].insert(remove_idx);
-        indices[lastElement].erase(data.size() - 1);  
-
-        data.pop_back();  
-
-        if (indices[val].empty()) {
+        data[removingIndex] = lastElement;
+        indices[lastElement].insert(removingIndex);
+        indices[lastElement].erase(data.size() - 1);
+        data.pop_back();
+        if(indices[val].empty()){
             indices.erase(val);
         }
-
         return true;
     }
-
+    
     int getRandom() {
-        int randomIndex = rand() % data.size();  
-        return data[randomIndex];  
+        int randomIndex = rand()%data.size();
+        return data[randomIndex];
     }
 };
 
