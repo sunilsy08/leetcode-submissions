9f03dc34770d625a763023817d5be88acac171e3
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-05 12:59:13 +0530

    Time: 155 ms (84.48%), Space: 101.9 MB (85.23%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,40 +1,42 @@
 class RandomizedSet {
 public:
-    unordered_map<int, int> m;  // Map from value to its index in the deque
-    deque<int> dq;  // Deque to store the elements
-    
-    RandomizedSet() {}
+    vector<int>data;
+    unordered_map<int,int>index;
+    RandomizedSet() {
+        
+    }
     
     bool insert(int val) {
-        if (m.find(val) == m.end()) {
-            dq.push_back(val);  // Insert element at the back of the deque
-            m[val] = dq.size() - 1;  // Store the index of the new element
+        if(index.find(val) == index.end()){
+            data.push_back(val);
+            index[val] = data.size()-1;
             return true;
         }
         return false;
     }
     
     bool remove(int val) {
-        if (m.find(val) == m.end()) {
+        if(index.find(val) == index.end()){
             return false;
         }
-        
-        // Get the index of the element to remove
-        int idx = m[val];
-        // Swap the element with the last element in the deque
-        dq[idx] = dq.back();
-        // Update the map for the element that was swapped
-        m[dq[idx]] = idx;
-        // Remove the last element
-        dq.pop_back();
-        // Erase the element from the map
-        m.erase(val);
-        
+        int ind = index[val];
+        swap(data[ind], data[data.size()-1]);
+        index[data[ind]] = ind;
+        data.pop_back();
+        index.erase(val);
         return true;
     }
     
     int getRandom() {
-        int randomIndex = rand() % dq.size();  // Generate a random index
-        return dq[randomIndex];  // Directly access the element at the random index
+        int randomIndex = rand() % data.size();
+        return data[randomIndex];
     }
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
