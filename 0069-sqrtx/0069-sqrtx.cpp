2194929415a32d56010072e9367d9ffb88079b04
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-06 18:41:23 +0530

    Time: 5 ms (34.83%), Space: 8.3 MB (71.46%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,13 +5,14 @@ public:
         int ans;
         while(l<=r){
             int mid = l + (r-l)/2;
-            if(x/mid >= mid){
+            if(mid == x/mid) return mid;
+            if(x/mid > mid){
                 ans = mid;
                 l = mid+1;
             } else {
                 r = mid-1;
             }
         }
-        return ans;
+        return r;
     }
 };
