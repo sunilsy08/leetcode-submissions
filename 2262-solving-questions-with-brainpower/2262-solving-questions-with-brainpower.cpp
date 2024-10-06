8c3a21f85ab5f8b07502be337a77859cea18e016
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-06 18:08:01 +0530

    Time: 250 ms (40.19%), Space: 119 MB (93.26%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -10,31 +10,20 @@ public:
     }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long>dp(n, -1);
-        // if(n<=3){
-        //     int ans = INT_MIN;
-        //     for(int i=0; i<n; i++){
-        //         ans = max(ans, questions[i][0]);
-        //     }
-        // }
-
-        // vector<long long>dp(n,0);
-        // // dp[0] = questions[0][0];
-        // // dp[1] = questions[1][0];
-        // // dp[2] = questions[2][0];
-        // for(int i=n-1; i>=0; i--){
-        //     int nextQuestion = i + questions[i][1];
-        //     long long nextQuestionPoints = 0;
-        //     if(nextQuestion < n) nextQuestionPoints = dp[nextQuestionPoints];
-        //     long long r = i!= n-1 ? dp[i+1] : 0;
-        //     long long l = questions[i][0] + nextQuestionPoints;
-        //     dp[i] = max(l,r);
-        // }
-        // long long ans = INT_MIN;
-        // for(int i=0; i<n; i++){
-        //     ans = max(ans, dp[i]);
-        // }
-        // return ans;
-        return helper(questions, 0, dp);
+        vector<long long> dp(n + 1, 0); 
+        
+        for (int i = n - 1; i >= 0; i--) {
+            long long skipCurrent = dp[i + 1];  
+            int nextQuestion = i + questions[i][1] + 1;
+            long long takeCurrent = questions[i][0];  
+            if (nextQuestion < n) {
+                takeCurrent += dp[nextQuestion];
+            }
+            
+            dp[i] = max(takeCurrent, skipCurrent);
+        }
+        
+        return dp[0]; 
+        // return helper(questions, 0, dp);
     }
 };
