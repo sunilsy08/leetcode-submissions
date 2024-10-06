fd0070fd1f0d7070fa8cc57b9f1c3a6377473d21
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-06 20:36:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        unsigned opencount = 0;
-        unsigned starcount = 0;
-        unsigned closecount = 0;
+        int opencount = 0;
+        int starcount = 0;
+        int closecount = 0;
         int l=0;
-        while(l<s.size()){
-            if(s[l] == '('){
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(' || s[i] == '*'){
                 opencount++;
-            } else if(s[l] == '*'){
-                starcount++;
             } else {
-                closecount++;
-                if(closecount > opencount){
-                    if(starcount == 0){
-                        return false;
-                    } else {
-                        starcount--;
-                        opencount++;
-                    }
-                }
+                opencount--;
             }
-            l++;
         }
-        return opencount == closecount;
+        if(opencount <0) return false;
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i] == ')' || s[i] == '*'){
+                closecount++;
+            } else {
+                closecount--;
+            }
+        }
+        if(closecount <0) return false;
+        return true;
     }
 };
