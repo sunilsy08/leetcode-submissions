40a81adf6246d7bd806163d5f610261ac3da6d6d
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-08 23:11:25 +0530

    Time: 44 ms (99.4%), Space: 32.7 MB (79.33%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int openCount = 0;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
-        for(char bracket : s){
-            if(bracket == '['){
-                openCount++;
+        int balance = 0, maxImbalance = 0;
+        
+        for(char bracket : s) {
+            if (bracket == '[') {
+                balance++; 
             } else {
-                if(openCount > 0){
-                    openCount--;
-                }
+                balance--; 
             }
+            maxImbalance = min(maxImbalance, balance);
         }
-        return (openCount + 1)/2;
+        
+        return (-maxImbalance + 1) / 2;
     }
 };
