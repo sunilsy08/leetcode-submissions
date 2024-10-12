f8144d509172215f5f29067733b041154164dd86
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 21:53:36 +0530

    Time: 12 ms (50.28%), Space: 21.4 MB (14.69%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        bool merged = false;
         int newStart = newInterval[0];
         int newEnd = newInterval[1];
-        bool merged = false;
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
+        for(auto interval : intervals){
 
-        for(auto interval: intervals){
-            int start = interval[0];
-            int end = interval[1];
+            int currStart = interval[0];
+            int currEnd = interval[1];
 
-            if(newEnd < start){
-                if(!merged){
-                    ans.push_back({newStart, newEnd});
-                    merged = true;
-                }
-                ans.push_back({start,end});
+            if(!merged && newEnd < currStart){
+                ans.push_back({newStart, newEnd});
                 merged = true;
-            }
-            else if(end >= newStart){
-                if(!merged){
-                    newStart = min(newStart, start);
-                    newEnd = max(newEnd, end);
-                }else {
-                    ans.push_back({start,end});
-                }
+                ans.push_back({currStart, currEnd});
+            } else if(!merged && currEnd >= newStart){
+                newStart = min(currStart, newStart);
+                newEnd = max(currEnd, newEnd);
             } else {
-                ans.push_back({start,end});
+                ans.push_back({currStart, currEnd});
             }
         }
         if(!merged){
