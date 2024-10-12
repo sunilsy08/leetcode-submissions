5c56688a69705ddee6630f045d363acb40c0aacb
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 22:11:18 +0530

    Time: 22 ms (26.05%), Space: 32.5 MB (5.1%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -18,11 +18,29 @@ public:
         return root->val == subRoot->val && isSame(root->left, subRoot->left) 
         && isSame(root->right, subRoot->right);
     }
+    void encodeTree(TreeNode* root, string &s){
+        if(root == NULL){
+            s += "N";
+            return;
+        }
+        s += "#" + to_string(root->val);
+        s +=",";
+        encodeTree(root->left, s);
+        s +=",";
+        encodeTree(root->right, s);
+    }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == NULL && subRoot == NULL) return true;
-        if(root== NULL || subRoot == NULL) return false;
-        if(isSame(root, subRoot)) return true;
+        // if(root == NULL && subRoot == NULL) return true;
+        // if(root== NULL || subRoot == NULL) return false;
+        // if(isSame(root, subRoot)) return true;
 
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        // return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        string s1, s2;
+        encodeTree(root, s1);
+        encodeTree(subRoot, s2);
+        cout<<s1<<"\n";
+        cout<<s2<<"\n";
+        if(s1.find(s2) < s1.size()) return true;
+        return false;
     }
 };
