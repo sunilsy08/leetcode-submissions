a0fb6db1bfec6d8e2891e7133de43fdf4bd151f2
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 22:55:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (77.31%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,12 +3,15 @@ public:
     int hammingWeight(int n) {
         
         int count = 0;
+        // IF INTERVIEWER MODIFIES THAT THERE CAN BE NEGATIVE NUMBERS
+        unsigned int num = static_cast<unsigned int>(n);
+        // NOTE THAT SIMPLY MAKING IT POSITIVE WON'T WORK
 
         for(int i=0; i<32; i++){
-            int lastbit = n&1;
+            int lastbit = num&1;
 
             if(lastbit != 0) count++;
-            n = n>>1;
+            num = num>>1;
         }
         return count;
     }
