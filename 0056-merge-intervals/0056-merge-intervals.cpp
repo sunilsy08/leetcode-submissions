9522e1c9b63b2f1d8e90e97f7149e57050706164
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 20:33:43 +0530

    Time: 13 ms (98.07%), Space: 23 MB (75.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        stack<vector<int>>st;
+        vector<vector<int>>mergedIntervals;
+        int n = intervals.size();
+        if(n<=1) return intervals;
+        sort(intervals.begin(), intervals.end(), 
+            [](const vector<int>&v1, const vector<int>&v2){
+                return v1[0] < v2[0];
+            }
+        );
+        mergedIntervals.push_back(intervals[0]);
 
-        int curr = 0;
-
-        while(curr<intervals.size()){
-            if(st.empty()){
-                st.push({intervals[curr][0], intervals[curr][1]});
-                curr++;
+        for(int i=1; i<n; i++){
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            
+            if(mergedIntervals.back()[1] >= currStart ){
+                mergedIntervals.back()[0] = min(mergedIntervals.back()[0], currStart);
+                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], currEnd);
             } else {
-                if(st.top()[1] >= intervals[curr][0]){
-                    st.top()[1]= max(st.top()[1], intervals[curr][1]);
-                    curr++;
-                } else {
-                    st.push({intervals[curr][0], intervals[curr][1]});
-                    curr++;
-                }
+                mergedIntervals.push_back(intervals[i]);
             }
         }
-        vector<vector<int>>ans;
-        while(!st.empty()){
-            ans.push_back({st.top()[0], st.top()[1]});
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return mergedIntervals;
     }
 };
