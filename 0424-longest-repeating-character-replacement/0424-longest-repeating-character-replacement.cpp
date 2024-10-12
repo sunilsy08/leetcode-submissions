7599e743ead9e95e075d7744805a2a03a0e0c710
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 17:52:39 +0530

    Time: 9 ms (54.52%), Space: 9.7 MB (20.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -16,12 +16,14 @@ public:
         int n = s.size();
         int left = 0, right = 0;
         int ans = 0;
+        int maxFrequency = 0;
         while (right < n) {
             windowFreq[s[right]]++;
+            maxFrequency = max(maxFrequency, windowFreq[s[right]]);
 
             int totalChars = right - left + 1;
-            int maxOccuringCharFreq = getMaxOccuringCharFreq(windowFreq);
-            if (totalChars - maxOccuringCharFreq <= k) {
+            // int maxOccuringCharFreq = getMaxOccuringCharFreq(windowFreq);
+            if (totalChars - maxFrequency <= k) {
                 ans = max(ans, totalChars);
                 right++;
             } else {
