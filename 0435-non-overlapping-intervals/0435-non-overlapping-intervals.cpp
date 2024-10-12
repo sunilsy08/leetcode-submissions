a6ffdc8d4b84416a5add9bb021ffebae2ee253f6
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-12 19:25:53 +0530

    Time: 220 ms (92.29%), Space: 93.5 MB (92.68%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        stack<vector<int>>st;
-        int count =0;
-        for(auto interval : intervals){
-            if(st.empty()){
-                st.push(interval);
-                continue;
-            }
-            int lastStart = st.top()[0];
-            int lastEnd = st.top()[1];
-            int currStart = interval[0];
-            int currEnd = interval[1];
+        int count = 0;
+        if(intervals.size() == 0) return count;
+
+        // SORT ON THE BASIS OF ENDING
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+
+        int lastEnd = intervals[0][1];
+        for(int i=1; i<intervals.size(); i++){
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
 
             if(currStart < lastEnd){
                 count++;
-                if (currEnd < lastEnd) {
-                    // Replace the top of the stack with the current interval as it has a smaller end
-                    st.pop();
-                    st.push(interval);
-                }
             } else {
-                st.push(interval);
+                lastEnd = currEnd;
             }
-            
         }
         return count;
     }
