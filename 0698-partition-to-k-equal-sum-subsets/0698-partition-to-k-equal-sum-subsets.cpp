53f2fbecb3d92697234a872814e7fe4989296ae7
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-15 11:28:28 +0530

    Time: 2073 ms (13.28%), Space: 12.2 MB (45.99%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool KSubsetsWithTargetSumPossible(vector<int>& nums, int i,int currSum,  int k,vector<bool>&used, int target ){
-        if(k == 0) return true;
-        if(currSum == target) {
-            return KSubsetsWithTargetSumPossible(nums, 0, 0, k-1, used, target);
+ bool kSubsetsWithTargetPossible(vector<int>& nums, int i,int currSum,  int k,vector<bool>&used, int target ){
+        if(k==0){
+            return true;
+        }
+        if(currSum == target){
+            return kSubsetsWithTargetPossible(nums, 0, 0, k-1, used, target);
         }
 
         for(int st = i; st<nums.size(); st++){
             if(!used[st] && currSum + nums[st] <= target){
                 used[st] = true;
-                if(KSubsetsWithTargetSumPossible(nums, st+1, currSum + nums[st], k, used, target)){
+                if(kSubsetsWithTargetPossible(nums, st+1, currSum+nums[st], k, used, target)){
                     return true;
                 }
                 used[st] = false;
+
             }
-        }   
+        }
         return false;
  }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        sort(nums.rbegin(), nums.rend());
-        int n = nums.size();
-        int sum =  accumulate(nums.begin(), nums.end(), 0);
+        int sum = 0, n = nums.size();
+        for(int num: nums){
+            sum += num;
+        }
         if(sum%k != 0) return false;
-
         int target = sum/k;
+// IMP
         if(*max_element(nums.begin(), nums.end()) > target) return false;
-        vector<bool>used(n,false);   
-
-        return KSubsetsWithTargetSumPossible(nums, 0,0,k, used, target);
+        vector<bool>taken (n, false);
+        return kSubsetsWithTargetPossible(nums, 0, 0, k, taken, target);
     }
 };
