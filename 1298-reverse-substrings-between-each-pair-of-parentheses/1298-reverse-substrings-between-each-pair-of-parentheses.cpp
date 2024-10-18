02081dd950e2ce75886651cb922ea69a16816ebb
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-18 10:28:02 +0530

    Time: 0 ms (100%), Space: 8 MB (56.66%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,38 +1,64 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<string>st;
+        int n = s.size();
+        stack<int>openbracketIndex;
+        vector<int>bracketpair(n);
 
-        for(char ch : s){
-            string temp;
-            temp += ch;
-            if(ch == '('){
-                st.push("(");
-            } else if(ch >='a' && ch <='z'){
-                st.push(temp);
-            } else {
-                string rem = "";
-                while(st.top() != "("){
-                    rem += st.top();
-                    st.pop();
-                }
-                if(!st.empty() && st.top() == "(")
-                st.pop();
-                // if(!st.empty()){
-                    reverse(rem.begin(), rem.end());
-                    st.push(rem);
-                // } else {
-                //    return rem;
-                // }
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                openbracketIndex.push(i);
+            } else if(s[i] == ')'){
+                int j = openbracketIndex.top();
+                openbracketIndex.pop();
+                bracketpair[i] = j;
+                bracketpair[j] = i;
             }
         }
-
-        string rem = "";
-        while(!st.empty()){
-            rem += st.top();
-            st.pop();
+        string res = "";
+        for(int currIndex = 0, direction =1; currIndex < n; currIndex +=direction){
+            if(s[currIndex] == '(' || s[currIndex] == ')'){
+                currIndex = bracketpair[currIndex];
+                direction = -direction;
+            } else {
+                res += s[currIndex];
+            }
         }
-        reverse(rem.begin(), rem.end());
-        return rem;
+        return res;
     }
+    // string reverseParentheses(string s) {
+    //     stack<string>st;
+
+    //     for(char ch : s){
+    //         string temp;
+    //         temp += ch;
+    //         if(ch == '('){
+    //             st.push("(");
+    //         } else if(ch >='a' && ch <='z'){
+    //             st.push(temp);
+    //         } else {
+    //             string rem = "";
+    //             while(st.top() != "("){
+    //                 rem += st.top();
+    //                 st.pop();
+    //             }
+    //             if(!st.empty() && st.top() == "(")
+    //             st.pop();
+    //             // if(!st.empty()){
+    //                 reverse(rem.begin(), rem.end());
+    //                 st.push(rem);
+    //             // } else {
+    //             //    return rem;
+    //             // }
+    //         }
+    //     }
+
+    //     string rem = "";
+    //     while(!st.empty()){
+    //         rem += st.top();
+    //         st.pop();
+    //     }
+    //     reverse(rem.begin(), rem.end());
+    //     return rem;
+    // }
 };
