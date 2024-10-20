940ab657321dadb8f50ab27033f0b9b973d96a83
Sunil Yadav <38045888+sunilsy08@users.noreply.github.com>
2024-10-20 13:03:50 +0530

    Time: 1016 ms (77.95%), Space: 259.4 MB (84.62%) - LeetHub

--- 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -17,13 +17,25 @@ public:
     }
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
         unordered_set<int>toplen = findPossibleSides(vFences, n);
-        unordered_set<int>sidelen = findPossibleSides(hFences, m);
+        // unordered_set<int>sidelen = findPossibleSides(hFences, m);
+        hFences.push_back(1);
+        hFences.push_back(m);
+
         int ans = -1;
-        for(auto side: toplen){
-            if(side > 0 && sidelen.count(side) > 0){
-                ans = max(ans, side);
+
+        for(int i=0; i<hFences.size(); i++){
+            for(int j=i+1; j<hFences.size(); j++){
+                int side = abs(hFences[j] - hFences[i]);
+                if(side != 0 && toplen.count(side) > 0){
+                    ans = max(ans, side);
+                }
             }
         }
+        // for(auto side: toplen){
+        //     if(side > 0 && sidelen.count(side) > 0){
+        //         ans = max(ans, side);
+        //     }
+        // }
         long long mod = 1e9 + 7;
         long res = ((long)ans*ans)%(mod);
         return ans == -1 ? ans : res;
